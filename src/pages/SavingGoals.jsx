c9a2c53bb993834616b6ca2b9490b5b28c18ad7e
Luke <lukesnapacc@gmail.com>
2024-04-04 15:14:54 +0200

    Add drawer component and display additional savings goals

--- src/pages/SavingGoals.jsx
+++ src/pages/SavingGoals.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { Drawer } from 'vaul';
+import { v4 as uuidv4 } from 'uuid';
 import SavingsInput from '../components/inputs/SavingsInputs';
 import ProgressBarMoney from '../components/bars/ProgressBarMoney';
 
@@ -7,6 +9,7 @@ const SavingGoals = () => {
   const [preis, setPreis] = useState('');
   const [cards, setCards] = useState([]);
   const [error, setError] = useState('');
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     const storedCards = localStorage.getItem('cards');
@@ -19,11 +22,14 @@ const SavingGoals = () => {
     if (cards.length > 0) {
       localStorage.setItem('cards', JSON.stringify(cards));
     }
+    if (cards.length === 0) {
+      setOpen(false);
+    }
   }, [cards]);
 
   const handleClick = () => {
     if (sparziel && preis) {
-      setCards([...cards, { title: sparziel, body: preis }]);
+      setCards([...cards, { id: uuidv4(), title: sparziel, body: preis }]);
       setSparziel('');
       setPreis('');
       setError('');
@@ -32,11 +38,14 @@ const SavingGoals = () => {
     }
   };
 
-  const handleDelete = (index) => {
-    const newCards = [...cards];
-    newCards.splice(index, 1);
-    setCards(newCards);
-    localStorage.setItem('cards', JSON.stringify(newCards));
+  const handleDelete = (cardToDelete) => {
+    const index = cards.findIndex((card) => card.id === cardToDelete.id);
+    if (index > -1) {
+      const newCards = [...cards];
+      newCards.splice(index, 1);
+      setCards(newCards);
+      localStorage.setItem('cards', JSON.stringify(newCards));
+    }
   };
 
   const handlePriceChange = (e) => {
@@ -59,6 +68,12 @@ const SavingGoals = () => {
   const cigsSmoked = days * cigsPerDay;
   const packsSmoked = cigsSmoked / cigsPerPack;
   const moneySaved = packsSmoked * pricePerPack;
+
+  const sortedCards = [...cards].sort((a, b) => a.body - b.body);
+
+  const cardsToDisplay = sortedCards
+    .filter((card) => card.body > moneySaved)
+    .slice(0, 2);
 
   return (
     <div className='min-h-screen flex flex-col justify-center items-center mx-8'>
@@ -94,13 +109,13 @@ const SavingGoals = () => {
               Hinzufügen
             </button>
           </div>
-          {cards.map((card, index) => (
+          {cardsToDisplay.map((card, index) => (
             <div key={index} className='card shadow-lg relative'>
               <div className='card-body'>
                 <div className='flex justify-between items-center'>
                   <h2 className='card-title'>{card.title}</h2>
                   <button
-                    onClick={() => handleDelete(index)}
+                    onClick={() => handleDelete(card)}
                     className='text-red-500 bg-transparent'
                   >
                     X
@@ -117,6 +132,63 @@ const SavingGoals = () => {
               </div>
             </div>
           ))}
+          <div className='mb-4'></div>
+          <button
+            className={`
+            text-white font-bold py-2 px-4 rounded mx-auto block
+            ${
+              cards.length === 0
+                ? 'bg-zinc-300'
+                : 'bg-teal-400 hover:bg-teal-500'
+            }`}
+            onClick={setOpen}
+            disabled={cards.length === 0}
+          >
+            Mehr anzeigen
+          </button>
+          <Drawer.Root open={open} onOpenChange={setOpen} shouldScaleBackground>
+            <Drawer.Portal>
+              <Drawer.Overlay className='fixed inset-0 bg-black/40' />
+              <Drawer.Content className='flex flex-col rounded-t-[10px] h-[90%] mt-0 fixed bottom-0 left-0 right-0 mx-auto max-w-screen-xl'>
+                <div className='p-4 bg-gray-100 rounded-t-[10px] overflow-y-auto scrollbar-hide h-full'>
+                  <div className='mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8' />
+                  <div className='max-w-2xl mx-auto'>
+                    <Drawer.Title className='font-bold text-2xl text-center mb-4'>
+                      Sparziele:
+                    </Drawer.Title>
+                    <div className='grid grid-rows-1 gap-6'>
+                      {cards.map((card, index) => (
+                        <div key={index} className='card shadow-lg relative'>
+                          <div className='card-body'>
+                            <div className='flex justify-between items-center'>
+                              <h2 className='card-title'>{card.title}</h2>
+                              <button
+                                onClick={() => handleDelete(card)}
+                                className='text-red-500 bg-transparent'
+                              >
+                                X
+                              </button>
+                            </div>
+                            <p>
+                              Preis:{' '}
+                              {new Intl.NumberFormat('de-DE', {
+                                style: 'currency',
+                                currency: 'EUR',
+                              }).format(card.body)}
+                            </p>
+                            <ProgressBarMoney
+                              goal={card.body}
+                              current={moneySaved}
+                            />
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </Drawer.Content>
+            </Drawer.Portal>
+          </Drawer.Root>
         </div>
       </div>
     </div>
