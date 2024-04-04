093e510affc94422e6dab68d175027636e73e699
Luke <lukesnapacc@gmail.com>
2024-04-05 01:59:16 +0200

    Refactor time conversion function and update milestone cards

--- src/pages/health.jsx
+++ src/pages/health.jsx
@@ -81,11 +81,9 @@ const milestones = [
   },
 ];
 
-const now = Date.now();
+const dateOfReturn = localStorage.getItem('dateOfReturn');
+const currentDate = Date.now();
 const milestonesWithProgress = milestones.map((milestone) => {
-  const dateOfReturn = localStorage.getItem('dateOfReturn');
-  const currentDate = Date.now();
-
   if (dateOfReturn) {
     const returnDate = new Date(dateOfReturn);
     const elapsedTime = currentDate - returnDate;
@@ -110,30 +108,36 @@ const milestonesWithProgress = milestones.map((milestone) => {
 
 const nextTwoMilestones = milestonesWithProgress
   .filter((milestone) => {
-    const milestoneTime = now + milestone.minutes * 60 * 1000;
+    const milestoneTime = currentDate + milestone.minutes * 60 * 1000;
     return (
-      milestoneTime > now &&
+      milestoneTime > currentDate &&
       milestoneTime <
-        now + (100 - milestone.progress) * milestone.minutes * 60 * 1000
+        currentDate + (100 - milestone.progress) * milestone.minutes * 60 * 1000
     );
   })
   .sort((a, b) => a.minutes - b.minutes)
   .slice(0, 2);
 
 function convertMinutesToTimeFormat(minutes) {
-  const years = Math.floor(minutes / (365 * 24 * 60));
-  const months = Math.floor((minutes % (365 * 24 * 60)) / (30 * 24 * 60));
-  const weeks = Math.floor((minutes % (30 * 24 * 60)) / (7 * 24 * 60));
-  const days = Math.floor((minutes % (7 * 24 * 60)) / (24 * 60));
+  const timeUnits = [
+    { unit: 'Jahr', value: 365 * 24 * 60 },
+    { unit: 'Monat', value: 30 * 24 * 60 },
+    { unit: 'Woche', value: 7 * 24 * 60 },
+    { unit: 'Tag', value: 24 * 60 },
+    { unit: 'Stunde', value: 60 },
+    { unit: 'Minute', value: 1 },
+  ];
 
-  let timeString = '';
-  if (years > 0) timeString += `${years} Jahr${years > 1 ? 'en' : ''} `;
-  else if (months > 0)
-    timeString += `${months} Monat${months > 1 ? 'en' : ''} `;
-  else if (weeks > 0) timeString += `${weeks} Woche${weeks > 1 ? 'n' : ''} `;
-  else if (days > 0) timeString += `${days} Tag${days > 1 ? 'en' : ''}`;
-
-  return timeString;
+  for (const { unit, value } of timeUnits) {
+    const quantity = Math.floor(minutes / value);
+    if (quantity > 0) {
+      if (unit === 'Stunde' || unit === 'Woche' || unit === 'Minute') {
+        return `${quantity} ${unit}${quantity > 1 ? 'n' : ''}`;
+      } else {
+        return `${quantity} ${unit}${quantity > 1 ? 'en' : ''}`;
+      }
+    }
+  }
 }
 
 function Health() {
@@ -172,129 +176,21 @@ function Health() {
                   Meilensteine:
                 </Drawer.Title>
                 <div className='grid grid-rows-1 gap-6'>
-                  <Card
-                    title='Puls und Blutdruck'
-                    content={
-                      <>
-                        <b>Nach 20 Minuten:</b> Puls und Blutdruck sinken auf
-                        normale Werte. <ProgressBarTime minutes='20' />
-                      </>
-                    }
-                  />
-                  <Card
-                    title='Frischer Atem'
-                    content={
-                      <>
-                        <b>Nach 8 Stunden:</b> Das Kohlenmonoxid im Blut wurde
-                        abgebaut und der stinkende Raucheratem hat sich
-                        verflüchtigt. <ProgressBarTime minutes='480' />
-                      </>
-                    }
-                  />
-                  <Card
-                    title='Herzinfarktrisiko'
-                    content={
-                      <>
-                        <b>Nach 24 Stunden:</b> Jetzt schon senkt sich das
-                        Risiko einen Herzinfarkt zu bekommen.
-                        <ProgressBarTime minutes='1440' />
-                      </>
-                    }
-                  />
-                  <Card
-                    title='Sinne'
-                    content={
-                      <>
-                        <b>Nach 2 Tagen:</b> Nikotin ist aus dem Körper
-                        eliminiert. Langsam kehren der natürliche Geruchs- und
-                        Geschmakssinn wieder zurück.
-                        <ProgressBarTime minutes='2880' />
-                      </>
-                    }
-                  />
-                  <Card
-                    title='Atemwege'
-                    content={
-                      <>
-                        <b>Nach 3 Tagen:</b> Die Atemwege entspannen sich, was
-                        zu einer leichteren Atmung führt. Energie steigt.
-                        <ProgressBarTime minutes='4320' />
-                      </>
-                    }
-                  />
-                  <Card
-                    title='Durchblutung'
-                    content={
-                      <>
-                        <b>Nach 2 Wochen:</b> Durchblutung verbessert sich, was
-                        zu besserer Hautfarbe und -textur führt.
-                        <ProgressBarTime minutes='20160' />
-                      </>
-                    }
-                  />
-                  <Card
-                    title='Husten & Atemprobleme'
-                    content={
-                      <>
-                        <b>Nach 6 Monaten:</b> Husten und Atemprobleme gehen
-                        zurück. Flimmerhärchen in den Atemwegen beginnen, sich
-                        zu regenerieren, was das Infektionsrisiko verringert.
-                        <ProgressBarTime minutes='262800' />
-                      </>
-                    }
-                  />
-                  <Card
-                    title='Koronare Herzkrankheiten'
-                    content={
-                      <>
-                        <b>Nach 1 Jahr:</b> Das Risiko für koronare
-                        Herzkrankheiten halbiert sich im Vergleich zu einem
-                        Raucher.
-                        <ProgressBarTime minutes='525600' />
-                      </>
-                    }
-                  />
-                  <Card
-                    title='Krebs'
-                    content={
-                      <>
-                        <b>Nach 5 Jahren:</b> Das Risiko für Mund-, Rachen-,
-                        Speiseröhren- und Blasenkrebs sinkt um die Hälfte.
-                        <ProgressBarTime minutes='2628000' />
-                      </>
-                    }
-                  />
-                  <Card
-                    title='Lungenkrebs'
-                    content={
-                      <>
-                        <b>Nach 10 Jahren:</b> Das Risiko für Lungenkrebs
-                        halbiert sich im Vergleich zu einem Raucher.
-                        <ProgressBarTime minutes='5256000' />
-                      </>
-                    }
-                  />
-                  <Card
-                    title='Herzkrankheiten'
-                    content={
-                      <>
-                        <b>Nach 15 Jahren:</b> Das Risiko für Herzkrankheiten
-                        ist das gleiche wie bei einem Nichtraucher.
-                        <ProgressBarTime minutes='7884000' />
-                      </>
-                    }
-                  />
-                  <Card
-                    title='COPD'
-                    content={
-                      <>
-                        <b>Nach 20 Jahren:</b> Das Risiko für chronisch
-                        obstruktive Lungenerkrankungen (COPD) ist das gleiche
-                        wie bei einem Nichtraucher.
-                        <ProgressBarTime minutes='10512000' />
-                      </>
-                    }
-                  />
+                  {milestones.map((milestone, index) => (
+                    <Card
+                      key={index}
+                      title={milestone.title}
+                      content={
+                        <>
+                          <b>{`Nach ${convertMinutesToTimeFormat(
+                            milestone.minutes
+                          )}:`}</b>{' '}
+                          {milestone.description}{' '}
+                          <ProgressBarTime minutes={milestone.minutes} />
+                        </>
+                      }
+                    />
+                  ))}
                 </div>
               </div>
             </div>
