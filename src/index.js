b8a366db81b8f5d82015daf79eda5249a9ac779a
Luke <lukesnapacc@gmail.com>
2024-04-05 14:08:00 +0200

    Refactor routing and add authentication context

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+
 import './index.css';
+
 import App from './App';
+import Signup from './components/Signup';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <Router>
+    <AuthProvider>
+      <Routes>
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/dashboard' element={<App />} />
+        <Route exact path='/' element={<Navigate to='/dashboard' />} />
+      </Routes>
+    </AuthProvider>
+  </Router>
 );
