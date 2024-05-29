af4fbdc50865c83f27efcab36bdc72c7e96759ca
Luke <lukesnapacc@gmail.com>
2024-05-29 11:51:06 +0200

    refactor: Update Login component to use controlled form inputs

--- app/src/pages/auth/Login.jsx
+++ app/src/pages/auth/Login.jsx
@@ -1,78 +1,38 @@
-import React, { useRef, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { useState } from 'react';
 
-export default function Login() {
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const [error, setError] = useState('');
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
+export default function Signup() {
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+  });
 
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    setError('');
-    setLoading(true);
-
-    axios
-      .post('http://localhost:3001/login', {
-        email: emailRef.current.value,
-        password: passwordRef.current.value,
-      })
-      .then((result) => {
-        if (result.data === 'Success') navigate('/');
-        else setError(result.data);
-      })
-      .catch((e) => {
-        setError(e.message);
-      });
-
-    setLoading(false);
-  }
+  const loginUser = (event) => {
+    event.preventDefault();
+  };
 
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
-      <div className='w-full max-w-md p-8 space-y-4 bg-white rounded shadow-md'>
-        <h2 className='text-2xl font-bold text-center'>Login</h2>
-        {error && <p className='text-red-500'>{error}</p>}
-        <form onSubmit={handleSubmit}>
-          <div className='space-y-4'>
-            <div>
-              <label className='text-sm font-bold text-gray-600'>Email</label>
-              <input
-                type='email'
-                ref={emailRef}
-                required
-                className='w-full px-3 py-2 mt-1 text-gray-700 border rounded-md focus:outline-none focus:shadow-outline bg-white'
-              />
-            </div>
-            <div>
-              <label className='text-sm font-bold text-gray-600'>
-                Passwort
-              </label>
-              <input
-                type='password'
-                ref={passwordRef}
-                required
-                className='w-full px-3 py-2 mt-1 text-gray-700 border rounded-md focus:outline-none focus:shadow-outline bg-white'
-              />
-            </div>
-            <button
-              disabled={loading}
-              className='w-full px-3 py-2 text-white bg-blue-500 rounded-md focus:bg-blue-600 focus:outline-none'
-            >
-              Log In
-            </button>
-          </div>
-        </form>
-      </div>
-      <div className='mt-2 text-center'>
-        Need an account?{' '}
-        <Link to='/signup' className='text-blue-500 underline'>
-          Sign Up
-        </Link>
-      </div>
+    <div>
+      <form onSubmit={loginUser}>
+        <label>Email</label>
+        <input
+          type='email'
+          placeholder='enter email...'
+          value={data.email}
+          onChange={(event) => {
+            setData({ ...data, email: event.target.value });
+          }}
+        />
+        <label>Password</label>
+        <input
+          type='password'
+          placeholder='enter password...'
+          value={data.password}
+          onChange={(event) => {
+            setData({ ...data, password: event.target.value });
+          }}
+        />
+        <button type='submit'>Login</button>
+      </form>
     </div>
   );
 }
--- app/src/pages/auth/Signup.jsx
+++ app/src/pages/auth/Signup.jsx
@@ -1,93 +1,48 @@
-import React, { useRef, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { useState } from 'react';
 
-export default function SignUp() {
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const passwordConfirmRef = useRef();
-  const navigate = useNavigate();
-  const [error, setError] = useState('');
-  const [loading, setLoading] = useState(false);
+export default function Signup() {
+  const [data, setData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
 
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError('Passwords do not match');
-    }
-
-    setError('');
-    setLoading(true);
-
-    axios
-      .post('http://localhost:3001/register', {
-        email: emailRef.current.value,
-        password: passwordRef.current.value,
-      })
-      .then(() => {
-        navigate('/');
-      })
-      .catch((e) => {
-        setError(e.message);
-      });
-
-    setLoading(false);
-  }
+  const registerUser = (event) => {
+    event.preventDefault();
+  };
 
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
-      <div className='w-full max-w-md p-8 space-y-4 bg-white rounded shadow-md'>
-        <h2 className='text-2xl font-bold text-center'>Sign Up</h2>
-        {error && <p className='text-red-500'>{error}</p>}
-        <form onSubmit={handleSubmit}>
-          <div className='space-y-4'>
-            <div>
-              <label className='text-sm font-bold text-gray-600'>Email</label>
-              <input
-                type='email'
-                ref={emailRef}
-                required
-                className='w-full px-3 py-2 mt-1 text-gray-700 border rounded-md focus:outline-none focus:shadow-outline bg-white'
-              />
-            </div>
-            <div>
-              <label className='text-sm font-bold text-gray-600'>
-                Passwort
-              </label>
-              <input
-                type='password'
-                ref={passwordRef}
-                required
-                className='w-full px-3 py-2 mt-1 text-gray-700 border rounded-md focus:outline-none focus:shadow-outline bg-white'
-              />
-            </div>
-            <div>
-              <label className='text-sm font-bold text-gray-600'>
-                Passwort bestätigen
-              </label>
-              <input
-                type='password'
-                ref={passwordConfirmRef}
-                required
-                className='w-full px-3 py-2 mt-1 text-gray-700 border rounded-md focus:outline-none focus:shadow-outline bg-white'
-              />
-            </div>
-            <button
-              disabled={loading}
-              className='w-full px-3 py-2 text-white bg-blue-500 rounded-md focus:bg-blue-600 focus:outline-none'
-            >
-              Sign Up
-            </button>
-          </div>
-        </form>
-      </div>
-      <div className='mt-2 text-center'>
-        Already have an account?{' '}
-        <Link to='/login' className='text-blue-500 underline'>
-          Log In
-        </Link>
-      </div>
+    <div>
+      <form onSubmit={registerUser}>
+        <label>Name</label>
+        <input
+          type='text'
+          placeholder='enter name...'
+          value={data.name}
+          onChange={(event) => {
+            setData({ ...data, name: event.target.value });
+          }}
+        />
+        <label>Email</label>
+        <input
+          type='email'
+          placeholder='enter email...'
+          value={data.email}
+          onChange={(event) => {
+            setData({ ...data, email: event.target.value });
+          }}
+        />
+        <label>Password</label>
+        <input
+          type='password'
+          placeholder='enter password...'
+          value={data.password}
+          onChange={(event) => {
+            setData({ ...data, password: event.target.value });
+          }}
+        />
+        <button type='submit'>Signup</button>
+      </form>
     </div>
   );
 }
