f5a5032aebd0b67605f4ad0bd58aee9a8b8959dc
Luke <lukesnapacc@gmail.com>
2024-05-31 09:07:53 +0200

    chore: Add mongoose package and create User model

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+// database connection
+console.log('Connecting to database...');
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('Database connected');
+  })
+  .catch((err) => {
+    console.log('Error connecting to database', err);
+  });
 
 const app = express();
 
