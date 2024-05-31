e1466e3d4b69426c5564a417e1850525d29035e1
Luke <lukesnapacc@gmail.com>
2024-05-31 11:08:54 +0200

    refactor: Hash user password during registration

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,51 +1,53 @@
 const User = require('../models/user');
+const { hashPassword, comparePassword } = require('../helpers/auth');
 
 const test = (req, res) => {
-  res.json({ message: 'Test works!' });
+    res.json({ message: 'Test works!' });
 };
 
 const registerUser = async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-    // check if name was entered
-    if (!name) {
-      return res.json({
-        error: 'Name is required',
-      });
+    try {
+        const { name, email, password } = req.body;
+        // check if name was entered
+        if (!name) {
+            return res.json({
+                error: 'Name is required',
+            });
+        }
+
+        // check if password is good
+        if (!password || password.length < 6) {
+            return res.json({
+                error: 'Password is required and should be 6 characters long',
+            });
+        }
+
+        // check email
+        const exist = await User.findOne({ email });
+        if (exist) {
+            return res.json({
+                error: 'Email is taken',
+            });
+        }
+
+        const hashedPassword = await hashPassword(password);
+        // create user in database
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        return res.json(user);
+    } catch (error) {
+        console.log('Error on registerUser', error);
+        return res.json({
+            error: 'Error. Please try again.',
+        });
     }
-
-    // check if password is good
-    if (!password || password.length < 6) {
-      return res.json({
-        error: 'Password is required and should be 6 characters long',
-      });
-    }
-
-    // check email
-    const exist = await User.findOne({ email });
-    if (exist) {
-      return res.json({
-        error: 'Email is taken',
-      });
-    }
-
-    // create user
-    const user = await User.create({
-      name,
-      email,
-      password,
-    });
-
-    return res.json(user);
-  } catch (error) {
-    console.log('Error on registerUser', error);
-    return res.json({
-      error: 'Error. Please try again.',
-    });
-  }
 };
 
 module.exports = {
-  test,
-  registerUser,
+    test,
+    registerUser,
 };
