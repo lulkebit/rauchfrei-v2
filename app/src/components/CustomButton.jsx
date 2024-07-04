a63f8cf509141f4d09a9e0088cf66d183c5ca6b8
Luke <lukesnapacc@gmail.com>
2024-07-04 14:10:41 +0200

    refactor: Update CustomButton component to handle disabled state

--- app/src/components/CustomButton.jsx
+++ app/src/components/CustomButton.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const CustomButton = ({ children, onClick }) => {
+const CustomButton = ({ children, onClick, state }) => {
+    const buttonClasses = `text-white font-medium py-2 px-6 rounded-full text-sm transition-colors duration-200 ${
+        state
+            ? 'bg-gray-400 hover:bg-gray-400 cursor-not-allowed'
+            : 'bg-emerald-500 hover:bg-emerald-600'
+    }`;
+
     return (
-        <button
-            className='bg-emerald-500 hover:bg-emerald-600 text-white font-medium py-2 px-6 rounded-full text-sm transition-colors duration-200'
-            onClick={onClick}
-        >
+        <button className={buttonClasses} onClick={onClick} disabled={state}>
             {children}
         </button>
     );
