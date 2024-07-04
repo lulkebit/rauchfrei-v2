5acf19c39898964e339c1669c102985f900b60fa
Luke <lukesnapacc@gmail.com>
2024-07-04 19:02:19 +0200

    refactor: Update SmokeFreeTimeCard component with countdown timer display

--- app/src/components/cards/SmokeFreeTimeCard.jsx
+++ app/src/components/cards/SmokeFreeTimeCard.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Card from './Card';
 import { useElapsedTime } from '../useElapsedTime';
 
 function SmokeFreeTimeCard() {
@@ -13,21 +12,63 @@ function SmokeFreeTimeCard() {
         seconds %= 3600;
         const minutes = Math.floor(seconds / 60);
         const remainingSeconds = seconds % 60;
-        return `${days} Tage ${hours} Stunden ${minutes} Minuten ${remainingSeconds} Sekunden`;
+        return { days, hours, minutes, remainingSeconds };
     };
 
+    const time = formatTime(timeElapsed);
+
     return (
-        <Card
-            title='Rauchfreie Zeit'
-            content={
-                typeof timeElapsed === 'number' && (
-                    <>
-                        Du bist <b>{formatTime(timeElapsed)}</b> rauchfrei!
-                        Weiter so!
-                    </>
-                )
-            }
-        />
+        <div className='flex items-start justify-center w-full gap-4 count-down-main'>
+            <div className='timer w-16'>
+                <div>
+                    <h3 className='countdown-element days font-manrope font-semibold text-2xl text-gray-700 text-center'>
+                        {time.days}
+                    </h3>
+                </div>
+                <p className='text-sm font-normal text-gray-700 mt-1 text-center w-full'>
+                    Tage
+                </p>
+            </div>
+            <h3 className='font-manrope font-semibold text-2xl text-gray-700'>
+                :
+            </h3>
+            <div className='timer w-16'>
+                <div>
+                    <h3 className='countdown-element hours font-manrope font-semibold text-2xl text-gray-700 text-center'>
+                        {time.hours}
+                    </h3>
+                </div>
+                <p className='text-sm font-normal text-gray-700 mt-1 text-center w-full'>
+                    Stunden
+                </p>
+            </div>
+            <h3 className='font-manrope font-semibold text-2xl text-gray-700'>
+                :
+            </h3>
+            <div className='timer w-16'>
+                <div>
+                    <h3 className='countdown-element minutes font-manrope font-semibold text-2xl text-gray-700 text-center'>
+                        {time.minutes}
+                    </h3>
+                </div>
+                <p className='text-sm font-normal text-gray-700 mt-1 text-center w-full'>
+                    Minuten
+                </p>
+            </div>
+            <h3 className='font-manrope font-semibold text-2xl text-gray-700'>
+                :
+            </h3>
+            <div className='timer w-16'>
+                <div>
+                    <h3 className='countdown-element seconds font-manrope font-semibold text-2xl text-gray-700 text-center'>
+                        {time.remainingSeconds}
+                    </h3>
+                </div>
+                <p className='text-sm font-normal text-gray-700 mt-1 text-center w-full'>
+                    Sekunden
+                </p>
+            </div>
+        </div>
     );
 }
 
