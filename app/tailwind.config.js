9014d4ccd3f395c8e5110494054c91ff7ab0c97f
Luke <lukesnapacc@gmail.com>
2024-07-04 19:25:28 +0200

    fix: Update tailwind.config.js to include './src/pages/auth/*.{js,jsx}'

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/*.{html,js}',
-    './src/components/*.{js,jsx}',
-    './src/components/cards/*.{js,jsx}',
-    './src/pages/*.{js,jsx}',
-  ],
-  plugins: [require('daisyui')],
+    content: [
+        './src/*.{html,js}',
+        './src/components/*.{js,jsx}',
+        './src/components/cards/*.{js,jsx}',
+        './src/pages/*.{js,jsx}',
+        './src/pages/auth/*.{js,jsx}',
+    ],
+    plugins: [require('daisyui')],
 };
